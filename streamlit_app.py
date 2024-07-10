b77859da807b142da9ded363e4899d8f367d9f39
Yunus Emre <61706944+suskun354@users.noreply.github.com>
2024-07-10 14:30:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=True, sheet_name='Sheet1')
-    writer.close()  # writer.save() yerine writer.close() kullanın
+    writer.close()  
     processed_data = output.getvalue()
     return processed_data
 
